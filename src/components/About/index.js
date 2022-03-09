9f86a7ca46347f277e5cf23cbfb9ae2cbe47c514
MFG <macfglenn@gmail.com>
2022-03-09 17:04:11 -0500

    initialized files

---
+++ src/components/About/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import coverImage from "../../assets/cover/cover-image.jpg";
+
+function About() {
+  return (
+    <section className="my-5">
+      <h1 id="about">Who am I?</h1>
+      <p>I am a student learning how to code in React! I also have knowledge of many other full stack languages like HTML, CSS, and JS. Looking for work and collabs, use the contact section to learn more.</p>
+    </section>
+  );
+}
+
+export default About;
